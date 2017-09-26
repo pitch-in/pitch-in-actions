27de7ea03019bc666c59eb165b1248eaffdfda47
Will Ockelmann-Wagner <wow@carbonfive.com>
2017-09-26 17:57:55 -0400

    add basic action form

--- src/app/action/action/action.component.ts
+++ src/app/action/action/action.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import * as moment from 'moment';
@@ -6,16 +6,24 @@ import * as moment from 'moment';
 import { Action } from '../action.model';
 import { Goal } from 'app/goal/goal.model';
 
+import { ActionFormBuilderService } from './../action-form-builder.service';
+
 @Component({
   selector: 'pi-action',
   templateUrl: 'action.component.html',
   styleUrls: ['action.component.scss']
 })
-export class ActionComponent {
+export class ActionComponent implements OnChanges {
   @Input() actionForm: FormGroup;
   @Input() goal: Goal;
 
-  constructor() {}
+  form: FormGroup;
+
+  constructor(private actionFormBuilder: ActionFormBuilderService) {}
+
+  ngOnChanges() {
+    this.form = this.actionFormBuilder.build(this.action);
+  }
 
   get action(): Action {
     return this.actionForm.value;
@@ -25,7 +33,20 @@ export class ActionComponent {
     return moment(this.goal.deadline);
   }
 
-  get startByDate(): moment.Moment {
-    return this.goalDeadline.subtract(this.action.startDaysBefore, 'days');
+  get startByDate(): string {
+    return dymamicDate(this.form, this.goalDeadline, 'startDaysBefore');
   }
+
+  get finishByDate(): string {
+    return dymamicDate(this.form, this.goalDeadline, 'finishDaysBefore');
+  }
+}
+
+function dymamicDate(
+  form: FormGroup,
+  goalDeadline: moment.Moment,
+  dateType: string
+): string {
+  console.log(form.value);
+  return goalDeadline.subtract(form.value[dateType], 'days').format('m/d/YYYY');
 }
